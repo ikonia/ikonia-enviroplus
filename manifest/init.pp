29263ed01954937008eb1cc47cf442a44d64a08e
Matt Darcy <matt.darcy@aviva.com>
2025-08-29 19:49:59 +0100

    Create init.pp

---
+++ manifest/init.pp
@@ -0,0 +1,15 @@
+#
+# Enviro+ Puppet module
+#
+# @summary this is the main class, used configure Enviro+.
+#
+# @param no parameters yet
+
+class enviroplus (
+  Array                                                $libvirt_package_names = [],
+  String                                               $enviro_group          = 'enviro',
+  String                                               $enviro_user           = 'enviro',
+  Boolean                                              $enble_prometheus      = false,
+  Boolean                                              $install_prometheus     = false,
+
+)
